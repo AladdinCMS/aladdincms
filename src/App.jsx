faa21f6c1c2bb2998733a41961e495358ef3db09
MAC <mac@MACs-MacBook-Pro-2.local>
2025-03-08 15:46:37 +0000

    feat: created the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
+import Volunteer from "./pages/volunteer/volunteer";
+import Contact from "./pages/external/page";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <main>
         <BrowserRouter>
+       
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/volunteer" element={<Volunteer />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </main>
     </>
